6bdf82a8b86d8bbff00f22b2c2a892e0a65ae0d2
Lexie Zhong <lexie.zhong@sap.com>
2024-05-03 10:42:37 +0800

    graph island

---
+++ graph/src/com/lc/graph/island/MaxAreaOfIsland_695.java
@@ -0,0 +1,67 @@
+package com.lc.graph.island;
+
+public class MaxAreaOfIsland_695 {
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}
+        };
+        System.out.println(maxAreaOfIsland(grid));
+    }
+
+    static int maxArea = 0;
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(isIsland(grid,i,j)) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private static int dfs(int[][] grid, int i, int j) {
+        if(!inArea(grid, i,j)) return 0;
+        if(isVisited(grid,i,j)) return 0;
+        if(isOcean(grid, i,j)) return 0;
+
+        //if(isIsland(grid,i,j)) return 1;
+
+        // set to visit
+        setToVisited(grid, i, j);
+
+        return 1+ dfs(grid,i-1,j)+dfs(grid,i+1,j) + dfs(grid,i,j-1)+dfs(grid,i,j+1);
+    }
+
+    private static boolean isIsland(int[][] grid, int i, int j) {
+        return grid[i][j] == 1;
+    }
+
+    private static boolean isOcean(int[][] grid, int i, int j) {
+        return grid[i][j] == 0;
+    }
+
+    private static boolean isVisited(int[][] grid, int i, int j) {
+        return grid[i][j] == 2;
+    }
+
+    private static void setToVisited(int[][] grid, int i, int j) {
+        grid[i][j] = 2;
+    }
+
+    private static boolean inArea(int[][] grid, int i, int j) {
+        return i>=0 && i<grid.length && j >=0 && j<grid[0].length;
+    }
+}
---
+++ graph/src/com/lc/graph/island/NumberOfIslands_200.java
@@ -0,0 +1,67 @@
+package com.lc.graph.island;
+
+public class NumberOfIslands_200 {
+    public static void main(String[] args) {
+//        char[][] grid = {
+//                {'1','1','1','1','0'},
+//                {'1','1','0','1','0'},
+//                {'1','1','0','0','0'},
+//                {'0','0','0','0','0'}
+//        };
+        char[][] grid = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+        System.out.println(numIslands(grid));
+    }
+
+    static int[][] dir = {{-1,0}, {0,1}, {1,0}, {0,-1}};  // define four directions
+    static boolean[][] visited;
+    static int cnt = 0;
+
+    public static int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        visited = new boolean[m][n];
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == '1' && !isVisited(i,j)) {
+                    dfs(grid, i, j);
+                    ++cnt;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if(!isValidPosition(grid, i, j) || !isIsland(grid, i,j) || isVisited(i,j)) return;
+
+        visited[i][j] = true;
+
+        for(int k=0;k<dir.length;k++) {
+            int ni = i+dir[k][0];
+            int nj = j+dir[k][1];
+            dfs(grid, ni, nj);
+        }
+
+    }
+
+    private static boolean isValidPosition(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return i >=0 && i<m && j>=0 && j<n;
+    }
+
+    private static boolean isIsland(char[][] grid, int i, int j) {
+        return grid[i][j] == '1';
+    }
+
+    private static boolean isVisited(int i, int j) {
+        return visited[i][j];
+    }
+}
