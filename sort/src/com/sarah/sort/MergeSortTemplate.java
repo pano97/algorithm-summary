1fc73d83c549c1ee2f78d6243ed050f6cacb963b
Lexie Zhong <lexie.zhong@sap.com>
2024-07-20 23:34:49 +0800

    reverse pair

--- sort/src/com/sarah/sort/MergeSortTemplate.java
+++ sort/src/com/sarah/sort/MergeSortTemplate.java
@@ -7,13 +7,18 @@ import java.util.Arrays;
  * */
 public class MergeSortTemplate {
     public static void main(String[] args) {
-        int[] nums = {0,-1,-3,2,6,3,2};
+//        int[] nums = {0,-1,-3,2,6,3,2};
+//
+//        nums = MergeSortTemplate.findReversePair(nums);
+//
+//        for(int num: nums)
+//            System.out.print(num+" ");
+//        System.out.println();
 
-        nums = MergeSortTemplate.sort_2(nums);
-
-        for(int num: nums)
-            System.out.print(num+" ");
-        System.out.println();
+        // 归并排序，测试逆序对
+        int[] nums = {1,2,3,4};
+        System.out.println(findReversePair(nums));
+        System.out.println(findReversePair_2(nums));
     }
 
     /**
@@ -95,4 +100,66 @@ public class MergeSortTemplate {
         return copy;
     }
 
+
+    /**
+     * 求逆序对个数, 归并排序
+     * input = [3,1,2,5,0]
+     * */
+    public static int findReversePair(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+
+        // copy origin nums
+        int[] copy = Arrays.copyOfRange(nums, 0, n);
+        int cnt = 0; // 逆序对个数
+
+        // 归并排序，两层循环
+        // 第一层循环seg, 代表group大小
+        for(int seg = 1; seg < n; seg+=seg) {
+            // 第二层循环，代表group中的每个起点
+            for(int start = 0; start < n; start += seg * 2) {
+                // 定义low, mid, high
+                int low = start, mid = Math.min(start + seg, n), high = Math.min(start + seg*2, n);
+
+                int start1 = low, end1 = mid-1;
+                int start2 = mid, end2 = high-1;
+                int k = start;
+                // 开始合并
+                while(start1 <= end1 && start2 <= end2) {
+                    if(nums[start1] <= nums[start2]) copy[k++] = nums[start1++];
+                    else {
+                        // nums[start1] > nums[start2], 逆序对出现
+                        cnt += end1 - start1 + 1;
+                        copy[k++] = nums[start2++];
+                    }
+                }
+
+                // 有剩余
+                while(start1 <= end1) copy[k++] = nums[start1++];
+                while(start2 <= end2) copy[k++] = nums[start2++];
+            }
+            // 物归原主
+            nums = Arrays.copyOfRange(copy, 0, n);
+        }
+        return cnt;
+    }
+
+    /**
+     * 暴力法
+     * */
+    public static int findReversePair_2(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int cnt = 0;
+
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(nums[i] > nums[j]) {
+                    ++cnt;
+                    //System.out.println("i="+i+" j="+j);
+                }
+            }
+        }
+        return cnt;
+    }
 }
